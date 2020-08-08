9f50b867af28659b17e9c1898312195b18ed9164
thejazzid <26236896+thejazzid@users.noreply.github.com>
2020-08-08 14:04:43 -0400

    Update load_tb_disabler.sh

--- load_tb_disabler.sh
+++ load_tb_disabler.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-kextstat |grep com.rugarciap.DisableTurboBoost || kextunload -v /Library/Extensions/DisableTurboBoost.64bits.kext
+kextstat |grep com.rugarciap.DisableTurboBoost && kextunload -v /Library/Extensions/DisableTurboBoost.64bits.kext
 kextload -v /Library/Extensions/DisableTurboBoost.64bits.kext
