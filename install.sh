e38d8e3454c550ab54d8653b549d96f934bdd60c
Alexey Shvarev <ashvarev@aligntech.com>
2024-04-11 10:25:31 -0400

    removed bins

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#unzip TurboBoostSwitcher.app.zip
+unzip 2.13.zip
 #cp -r TurboBoostSwitcher.app/Contents/Resources/DisableTurboBoost.64bits.kext /Library/Extensions
 cp -r 2.13/tbswitcher_resources/DisableTurboBoost.64bits.kext /Library/Extensions
 chown -R root:wheel /Library/Extensions/DisableTurboBoost.64bits.kext
