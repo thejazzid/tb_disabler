c69db1277aac1a3d1fda26f3cd4cc975f91c1e58
Alexey Shvarev <ashvarev@aligntech.com>
2024-04-11 10:06:29 -0400

    new version

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-unzip TurboBoostSwitcher.app.zip
-cp -r TurboBoostSwitcher.app/Contents/Resources/DisableTurboBoost.64bits.kext /Library/Extensions
+#unzip TurboBoostSwitcher.app.zip
+#cp -r TurboBoostSwitcher.app/Contents/Resources/DisableTurboBoost.64bits.kext /Library/Extensions
+cp -r 2.13/tbswitcher_resources/DisableTurboBoost.64bits.kext /Library/Extensions
 chown -R root:wheel /Library/Extensions/DisableTurboBoost.64bits.kext
 mkdir -p /Users/Shared/scripts
 cp -v load_tb_disabler.sh /Users/Shared/scripts
